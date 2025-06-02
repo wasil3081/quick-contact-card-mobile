5c52ded6c014601663b246ce532e337d59800e05
Wasil Anwar <wasil3081@users.noreply.github.com>
2025-06-02 06:01:08 +0500

    Update ContactCard.tsx

--- src/components/ContactCard.tsx
+++ src/components/ContactCard.tsx
@@ -26,7 +26,7 @@ const ContactCard = () => {
             className="hover:opacity-80 transition-opacity"
           >
             <img 
-              src="/lovable-uploads/fe4488e5-da78-4084-977d-dfb23a4f8b1f.png"
+              src="/lovable-uploads/5ab42a7b-e8c2-4729-8ed9-f11afd003bc3.png"
               alt="LinkedIn"
               className="w-8 h-8"
             />
